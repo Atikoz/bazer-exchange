e192e7027a97ad2079221292db7f55c6c191a5da
VVG <VVG@d.c>
2024-07-15 13:55:19 +0300

    fix bug cron job check reward

--- cron/minter/RewardBazerHUB.js
+++ cron/minter/RewardBazerHUB.js
@@ -2,7 +2,7 @@ const accrualRewards = require('../../function/rewardsMinter/checkReward');
 
 const CronJob = require('cron').CronJob;
 
-const rewardMinter = new CronJob('0 */1 * * * *', async () => {
+const rewardMinter = new CronJob('0 0 14 * * 1', async () => {
   try {
     await accrualRewards();
   } catch (error) {
