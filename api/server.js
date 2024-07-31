df76d7b10bc101747afc3279186e4519311ee3d0
VVG <VVG@d.c>
2024-07-31 12:43:10 +0300

    change listen port api

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use('/bot-api', authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
