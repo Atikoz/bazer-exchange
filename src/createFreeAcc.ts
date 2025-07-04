fa5d17aa1c28fdd91fd0551776e59c2f5006de25
VVG <VVG@d.c>
2025-07-04 14:59:51 +0300

    featch: add create new free acc fun

---
+++ src/createFreeAcc.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { connectToMongo } from './db/connectToMongo';
+import BalanceUser from './models/user/BalanceModel';
+import { FreeAccountService } from './service/user/FreeAccountService';
+
+
+const mongoUri = process.env.MONGO_URI as string;
+
+(async () => {
+  try {
+    await connectToMongo(mongoUri);
+
+    const countAcc = 20;
+
+    for (let i = 1; i < countAcc; i++) {
+      const result = await FreeAccountService.createNew()
+      console.log(`✅ Updated ${result} documents`);
+    }
+
+  } catch (error) {
+    console.error('MongoDB connection error :', error);
+    process.exit(1);
+  }
+
+})();
